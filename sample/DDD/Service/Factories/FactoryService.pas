2891b159d4609a8bd380d2387d7ddb8da2c3678a
Lindemberg <lindemberg@tecsoft.info>
2019-05-06 15:11:30 -0300

    Refatorar TFactoryService

--- sample/DDD/Service/Factories/FactoryService.pas
+++ sample/DDD/Service/Factories/FactoryService.pas
@@ -5,40 +5,43 @@ interface
 uses
     FactoryEntity,
     Service.Interfaces.Services.Servicebase,
-    infra.Interfaces.Repositorios.RepositoryBase, VCL.Dialogs, Sysutils, EF.Mapping.Base;
+    infra.Interfaces.Repositorios.RepositoryBase, VCL.Dialogs, Sysutils, EF.Mapping.Base, Domain.Entity.Cliente;
 
 type
    TFactoryService = class
    private
-     class function GetServiceCliente(E: string; var pRepository : IRepositoryBase<TEntitybase>): TEntitybase;
+     class function GetServiceCliente(E: string): IServiceBase<TCliente>;
    public
      class function GetService( E: string ):IServiceBase<TEntitybase>;
    end;
 
 implementation
 
-uses  FactoryRepository, Service.Base, EF.Mapping.AutoMapper, Domain.Entity.Cliente;//ServiceEntity;
+uses  FactoryRepository, Service.Base, EF.Mapping.AutoMapper;//ServiceEntity;
 
 { TFactoryService }
 
 class function TFactoryService.GetService(E: string): IServiceBase<TEntitybase>;
 var
-  Service     : IServiceBase<TEntitybase>;
-  Instance    : TEntitybase;
-  pRepository : IRepositoryBase<TEntitybase>;
+  Service  : IServiceBase<TEntitybase>;
 begin
-  Instance := GetServiceCliente(E, pRepository );
-  if Instance <> nil then
-  begin
-    Service :=  TServiceBase<TEntitybase>( Instance ).create(pRepository );
-    result:= Service;
-  end;
+  if E = 'Cliente' then
+     result   := GetServiceCliente(E ) as IServiceBase<TEntitybase>;
 end;
 
-class function TFactoryService.GetServiceCliente(E: string; var pRepository : IRepositoryBase<TEntitybase> ): TEntitybase;
+class function TFactoryService.GetServiceCliente(E: string ): IServiceBase<TCliente>;
+var
+  Instance    : TEntitybase;
+  pRepository : IRepositoryBase<TCliente>;
+  Service     : IServiceBase<TCliente>;
 begin
-  pRepository := TFactoryRepository.GetRepository<TCliente>(E);
-  result      := TAutoMapper.GetInstance<TCliente>( 'Service.'+E+'.TService'+E);
+  pRepository := TFactoryRepository.GetRepository<TCliente>(E) as IRepositoryBase<TCliente>;
+  Instance    := TAutoMapper.GetInstance<TCliente>( 'Service.'+E+'.TService'+E);
+  if Instance <> nil then
+  begin
+    Service :=  TServiceBase<TCliente>( Instance ).create(pRepository );
+    result:= Service;
+  end;
 end;
 
 end.
