053b499a45a736ef0fbaa6a5b53b21b7d4a7831f
Lindemberg Cortez <lindemberg.desenvolvimento@gmail.com>
2019-08-18 09:39:53 -0300

    Classe MigrationBuilder responsavel por montar os scripts de criação

---
+++ source/EF.Drivers.Migration.pas
@@ -0,0 +1,67 @@
+unit EF.Drivers.Migration;
+
+interface
+
+uses
+  EF.Mapping.Base, System.Classes, EF.Drivers.Connection, StrUtils;
+
+type
+   TMigrationBuilder = class
+   private
+     Path: string;
+     Filename:string;
+     FTableName: string;
+     FFDConn: FDConn;
+     function BuildScript<T:TEntityBase>(E:T;Name:string): string;
+   public
+     Script:TStringList;
+   type
+       TMethod = reference to procedure;
+     procedure CreateTable<T:TEntityBase>(out E:T; aTableName:string; Method: TMethod );
+     constructor Create(aFDConn: FDConn ; aPath:string);
+     destructor Destroy;override;
+   end;
+
+implementation
+
+{ TMigrationBuilder }
+
+uses EF.Mapping.AutoMapper, Vcl.Dialogs, System.SysUtils;
+
+function TMigrationBuilder.BuildScript<T>(E: T;Name:string):string;
+var
+  columns: string;
+begin
+  columns:= TAutoMapper.GetColumns(E);
+  result:= Format('Create Table %s ( %s )',[ Name, columns]);
+end;
+
+constructor TMigrationBuilder.Create(aFDConn: FDConn ;aPath: string);
+begin
+   Path := aPath;
+   FFDConn:= aFDConn;
+   filename:= Path+'\'+FormatDatetime('YYYYMMDDHHMNSS', now)+'.sql';
+   Script:=TStringList.Create;
+end;
+
+procedure TMigrationBuilder.CreateTable<T>(out E:T; aTableName:string; Method: TMethod );
+begin
+  try
+    E := T.Create;
+    Method;
+    Script.Add( BuildScript<T>(E,aTableName));
+    Script.Add(ifthen(FFDConn = fdMSSQL,'GO',';'));
+    Script.Add( Format('ALTER TABLE %s ADD PRIMARY KEY (ID)',[aTableName]) );
+    Script.Add(ifthen(FFDConn = fdMSSQL,'GO',';'));
+  finally
+    E.free;//Destroi o objeto
+  end;
+end;
+
+destructor TMigrationBuilder.Destroy;
+begin
+  Script.SaveToFile(filename);
+  Script.Free;
+end;
+
+end.
